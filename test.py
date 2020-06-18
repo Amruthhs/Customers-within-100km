7eda1bb0dc3991686eb99dee15b2382971d643c4
Amruth <amruth92hs@gmail.com>
2020-06-18 01:01:13 +0100

    Added unit test method
    Added unit test method

--- test.py
+++ test.py
@@ -11,12 +11,19 @@ class TestCustomerRecords(unittest.TestCase):
         test = CustomerRecords('Customer_list.txt', 100000, 53.339428, -6.257664)
         self.assertLess(test.get_distance(
             53.339428, -6.257664, 52.986375, -6.043701), 50000)
-
+        
     def test_read_file(self):
         """Assert that there are 16 correct answers in Customer_list.txt"""
 
         test = CustomerRecords('Customer_list.txt', 100000, 53.339428, -6.257664)
         self.assertEqual(len(test.read_file()), 16)
+
+    def test_measure_distance_method(self):
+        """Comparing the distance between hq and the entry in customer_list.txt"""
+
+        test = CustomerRecords('Customer_list.txt', 100000, 53.339428, -6.257664)
+        self.assertGreater(test.get_distance(
+            53.339428, -6.257664, 51.515314, -0.155163), 460)
 
 if __name__ == '__main__':
     unittest.main()
